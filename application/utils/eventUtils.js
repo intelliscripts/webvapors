323795907b2fc5e285f29edad1266a0983e19624
shivaprasad.manupadi <shivaprasad.manupadi@servicenow.com>
2019-12-24 14:41:46 +0530

    docs

--- application/utils/eventUtils.js
+++ application/utils/eventUtils.js
@@ -1,6 +1,6 @@
 var moment = require('moment-timezone');
 
-const INTERNAL_DATE = 'YYYY-MM-DD';
+const INTERNAL_DATE_TIME = 'YYYY-MM-DD HH:mm:ss';
 const CONTEXT_DATE = new moment().format('YYYY-MM-DD');
 
 function generateId() {
@@ -19,40 +19,70 @@ function getEvent(title, description, start, end, text_color, bg_color) {
     };
 }
 
-function generateEvents(contextMoment) {
-    const plus1Moment = contextMoment.clone().add(1, 'day').startOf('day');
-    //const minus2Moment = contextMoment.clone().add(-2, 'day').startOf('day');
-    const minus7Moment = contextMoment.clone().add(-7, 'day').startOf('day');
+let eventsCount = 0;
+const colors = ['#d2e7e3', '#e9e1f1', '#f3d9d2', '#FFECB3'];
 
-    const plus2Moment = contextMoment.clone().add(2, 'day').startOf('day');
-    const plus7Moment = contextMoment.clone().add(7, 'day').startOf('day');
-
-    const contextDateEvents = [
-        getEvent('event-1', 'sample description', contextMoment.format(INTERNAL_DATE) + ' 08:00:00', contextMoment.format(INTERNAL_DATE) + ' 10:00:00')
-    ];
-
-    const plus1DateEvents = [
-        getEvent('event-2', 'sample description', plus1Moment.format(INTERNAL_DATE) + ' 08:00:00', plus1Moment.format(INTERNAL_DATE) + ' 10:00:00')
-    ];
-
-    const multiDayEvents = [
-        getEvent('multi day event-4', 'sample description', minus7Moment.format(INTERNAL_DATE) + ' 08:00:00', plus7Moment.format(INTERNAL_DATE) + ' 10:00:00', '', '#d2e7e3'),
-        getEvent('multi day event-5', 'sample description', plus2Moment.format(INTERNAL_DATE) + ' 08:00:00', plus7Moment.format(INTERNAL_DATE) + ' 10:00:00', '', '#e9e1f1'),
-
-        getEvent('multi day event-6', 'sample description', minus7Moment.format(INTERNAL_DATE) + ' 08:00:00', plus7Moment.format(INTERNAL_DATE) + ' 10:00:00', '', '#f3d9d2'),
-        getEvent('multi day event-7', 'sample description', plus2Moment.format(INTERNAL_DATE) + ' 08:00:00', plus7Moment.format(INTERNAL_DATE) + ' 10:00:00', '', '#FFECB3')
-    ];
-
+function generateEventsForDate(dateMoment) {
+    const dateMoment1 = dateMoment.clone();
     const events = [
-        ...contextDateEvents,
-        ...plus1DateEvents,
-        ...multiDayEvents
-    ];
+    getEvent('event-' + eventsCount++, 'sample description', dateMoment1.add(1, 'hours').format(INTERNAL_DATE_TIME), dateMoment1.clone().add(5, 'hours').format(INTERNAL_DATE_TIME), '', colors[0]),
+    getEvent('event-' + eventsCount++, 'sample description', dateMoment1.add(7, 'hours').format(INTERNAL_DATE_TIME), dateMoment1.clone().add(8, 'hours').format(INTERNAL_DATE_TIME), '', colors[1]),
+        getEvent('event-' + eventsCount++, 'sample description', dateMoment1.add(5, 'hours').format(INTERNAL_DATE_TIME), dateMoment1.clone().add(8, 'hours').format(INTERNAL_DATE_TIME), '', colors[2]),
+        ];
     return events;
 }
 
+function generateEventsForMonth(contextMoment) {
+    let events = [];
+
+    const monthStart = contextMoment.clone().startOf('month').add(7, 'days');
+
+
+    events = events.concat(generateEventsForDate(monthStart));
+
+    monthStart.add(2, 'days');
+    events = events.concat(generateEventsForDate(monthStart));
+
+    monthStart.add(3, 'days');
+    events = events.concat(generateEventsForDate(monthStart));
+
+    monthStart.add(3, 'days');
+    events = events.concat(generateEventsForDate(monthStart));
+
+    monthStart.add(2, 'days');
+    events = events.concat(generateEventsForDate(monthStart));
+
+    monthStart.add(3, 'days');
+    events = events.concat(generateEventsForDate(monthStart));
+
+    monthStart.add(4, 'days');
+    events = events.concat(generateEventsForDate(monthStart));
+
+    return events;
+}
+
+function generateEvents(contextMoment) {
+
+    eventsCount = 1;
+    return [
+        ...generateEventsForMonth(contextMoment),
+        ...generateEventsForMonth(contextMoment.add(1, 'month')),
+        ...generateEventsForMonth(contextMoment.add(1, 'month')),
+        ...generateEventsForMonth(contextMoment.add(1, 'month')),
+        ...generateEventsForMonth(contextMoment.add(1, 'month')),
+        ...generateEventsForMonth(contextMoment.add(1, 'month')),
+        ...generateEventsForMonth(contextMoment.add(1, 'month')),
+        ...generateEventsForMonth(contextMoment.add(-7, 'month')),
+        ...generateEventsForMonth(contextMoment.add(-1, 'month')),
+        ...generateEventsForMonth(contextMoment.add(-1, 'month')),
+        ...generateEventsForMonth(contextMoment.add(-1, 'month')),
+        ...generateEventsForMonth(contextMoment.add(-1, 'month')),
+        ...generateEventsForMonth(contextMoment.add(-1, 'month')),
+    ]
+}
+
 function getEvents() {
-    const contextMoment = moment(CONTEXT_DATE, INTERNAL_DATE);
+    const contextMoment = moment(CONTEXT_DATE, INTERNAL_DATE_TIME);
     return generateEvents(contextMoment);
 }
 
