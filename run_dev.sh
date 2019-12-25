1499854fc4c91ffe289a9882be44269f94566a8d
shivaprasad.manupadi <shivaprasad.manupadi@servicenow.com>
2019-12-25 15:00:15 +0530

    cmd

--- run_dev.sh
+++ run_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export NODE_ENV=development
 export NODE_PORT=3009
-pm2 app.js
+pm2 start app.js
--- run_prod.sh
+++ run_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export NODE_ENV=production
 export NODE_PORT=80
-pm2 app.js
+pm2 start app.js
